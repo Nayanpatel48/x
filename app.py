116b2e0855c3f0a3b486bb0a4d3ddb59e2b7a003
NayanPatel48 <www.patelnayan187@gmail.com>
2025-07-11 22:58:08 +0530

    /

--- app.py
+++ app.py
@@ -100,6 +100,8 @@ def search_news():
     results = fetch_and_rank(query)
     return jsonify(results)
 
+import os
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
